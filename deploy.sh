9cfadc48dbc2f619c5671e519f605080c40fc228
Wesley Dai <slipyslidymaple@gmail.com>
2019-02-26 00:26:48 -0600

    Skip CI for prod builds

--- deploy.sh
+++ deploy.sh
@@ -9,20 +9,20 @@ git_commit() {
     git checkout --orphan prod
     timestamp=$(date "+%b %d %Y")
     git add bgm.min.json
-    git commit -m "Travis CI update: $timestamp (Build $TRAVIS_BUILD_NUMBER)"
+    git commit -m "Travis CI update: $timestamp (Build $TRAVIS_BUILD_NUMBER)" -m "[skip ci]"
 }
 
 git_push() {
     git remote rm origin
     git remote add origin https://${GITHUB_TOKEN}@github.com/maplestory-music/maplebgm-db.git > /dev/null 2>&1
-    git push origin prod --force --quiet
+    git push origin prod --force --quiet > /dev/null 2>&1
 }
 
 setup_git
 git_commit
 
 if [ $? -eq 0 ]; then
-    echo "Change in bgm.json detected"
+    echo "Change in bgm.json detected. Pushing to prod branch..."
     git_push
 else
     echo "No changes in bgm.json"
