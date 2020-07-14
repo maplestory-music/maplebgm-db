751f009ac691c3ebf640371037b891b720482455
Wesley Dai <slipyslidymaple@gmail.com>
2020-07-13 21:00:19 -0400

    Add test to enforce unique YouTube identifiers

--- bgm.test.js
+++ bgm.test.js
@@ -4,24 +4,31 @@ const bgmDatabase = require('./bgm');
 
 describe('bgm database', () => {
   test('should adhere to json schema', () => {
-    let ajv = new Ajv({allErrors: true});
-    let validate = ajv.compile(schema);
-    let valid = validate(bgmDatabase);
+    const ajv = new Ajv({ allErrors: true });
+    const validate = ajv.compile(schema);
+    const valid = validate(bgmDatabase);
     if (!valid) {
       console.error(validate.errors);
     }
     expect(valid).toEqual(true);
   });
   test('should be sorted by filename', () => {
-    let songNames = bgmDatabase.map(song => song.filename);
-    let sortedSongNames = bgmDatabase.map(song => song.filename);
-    sortedSongNames.sort((a, b) =>
-      a.localeCompare(b, 'en', {ignorePunctuation: true}));
+    const songNames = bgmDatabase.map((song) => song.filename);
+    const sortedSongNames = bgmDatabase
+      .map((song) => song.filename)
+      .sort((a, b) => a.localeCompare(b, 'en', { ignorePunctuation: true }));
     expect(songNames).toEqual(sortedSongNames);
   });
-  test('filename should be unique', () => {
-    let songNames = bgmDatabase.map(song => song.filename);
-    let distinctSongNames = new Set(songNames);
+  test('should have unique filenames', () => {
+    const songNames = bgmDatabase.map((song) => song.filename);
+    const distinctSongNames = new Set(songNames);
     expect(songNames.length).toEqual(distinctSongNames.size);
+  });
+  test('should have unique youtube identifiers', () => {
+    const youtube = bgmDatabase
+      .map((song) => song.youtube)
+      .filter((id) => id !== '');
+    const duplicates = youtube.filter((e, i, a) => a.indexOf(e) !== i);
+    expect(duplicates).toEqual([]);
   });
 });
