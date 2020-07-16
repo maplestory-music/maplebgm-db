1988efe208b292ec3a5a7c45ba80c6f31ba6a933
Wesley Dai <slipyslidymaple@gmail.com>
2020-07-16 00:12:26 -0400

    Modify tests to cover partitions

--- bgm.test.js
+++ bgm.test.js
@@ -1,33 +1,44 @@
 const Ajv = require('ajv');
+const fs = require('fs');
 const schema = require('./schema');
-const bgmDatabase = require('./bgm');
+const utils = require('./utils');
+
+const bgmDir = './bgm';
 
 describe('bgm database', () => {
   test('should adhere to json schema', () => {
-    const ajv = new Ajv({ allErrors: true });
-    const validate = ajv.compile(schema);
-    const valid = validate(bgmDatabase);
-    if (!valid) {
-      console.error(validate.errors);
-    }
-    expect(valid).toEqual(true);
+    const partitions = utils.traverseDirectory(bgmDir);
+    partitions.forEach((file) => {
+      const ajv = new Ajv({ allErrors: true });
+      const validate = ajv.compile(schema);
+      const valid = validate(JSON.parse(fs.readFileSync(file, 'utf8')));
+      if (!valid) {
+        console.error(`Failed validaton on ${file}`);
+        console.error(validate.errors);
+      }
+      expect(valid).toEqual(true);
+    });
   });
   test('should be sorted by filename', () => {
-    const songNames = bgmDatabase.map((song) => song.filename);
-    const sortedSongNames = bgmDatabase
-      .map((song) => song.filename)
-      .sort((a, b) => a.localeCompare(b, 'en', { ignorePunctuation: true }));
-    expect(songNames).toEqual(sortedSongNames);
+    const partitions = utils.traverseDirectory(bgmDir);
+    partitions.forEach((file) => {
+      const db = JSON.parse(fs.readFileSync(file, 'utf8'));
+      const songNames = db.map((song) => song.filename);
+      const sortedSongNames = db
+        .map((song) => song.filename)
+        .sort((a, b) => a.localeCompare(b, 'en', { ignorePunctuation: true }));
+      expect(songNames).toEqual(sortedSongNames);
+    });
   });
   test('should have unique filenames', () => {
-    const songNames = bgmDatabase.map((song) => song.filename);
+    const db = utils.getBgmDatabase(bgmDir);
+    const songNames = db.map((song) => song.filename);
     const distinctSongNames = new Set(songNames);
     expect(songNames.length).toEqual(distinctSongNames.size);
   });
   test('should have unique youtube identifiers', () => {
-    const youtube = bgmDatabase
-      .map((song) => song.youtube)
-      .filter((id) => id !== '');
+    const db = utils.getBgmDatabase(bgmDir);
+    const youtube = db.map((song) => song.youtube).filter((id) => id !== '');
     const duplicates = youtube.filter((e, i, a) => a.indexOf(e) !== i);
     expect(duplicates).toEqual([]);
   });
---
+++ utils.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+function traverseDirectory(dir) {
+  const files = [];
+  fs.readdirSync(dir).forEach((file) => {
+    const fullPath = path.join(dir, file);
+    if (fs.lstatSync(fullPath).isDirectory()) {
+      files.push(...traverseDirectory(fullPath));
+    } else {
+      files.push(fullPath);
+    }
+  });
+  return files;
+}
+
+function getBgmDatabase(dir) {
+  const bgmCollection = [];
+  traverseDirectory(dir)
+    .filter((fullPath) => path.extname(fullPath) === '.json')
+    .forEach((fullPath) => {
+      bgmCollection.push(...JSON.parse(fs.readFileSync(fullPath, 'utf8')));
+    });
+  return bgmCollection;
+}
+
+module.exports = {
+  getBgmDatabase,
+  traverseDirectory,
+};
