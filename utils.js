20d84570a7ec5813dc0d8eac64b8faec73d2c7a1
Wesley Dai <slipyslidymaple@gmail.com>
2021-12-12 19:32:16 -0600

    Support additional languages
    Metadata language support for Korean, Japanese, Chinese (Simplified) and Chinese (Traditional)

--- utils.js
+++ utils.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 
+const locales = ['ko', 'ja', 'zh-CN', 'zh-TW'];
+
 function traverseDirectory(dir) {
   const files = [];
+  if (!fs.existsSync(dir)) return [];
   fs.readdirSync(dir).forEach((file) => {
     const fullPath = path.join(dir, file);
     if (fs.lstatSync(fullPath).isDirectory()) {
@@ -24,8 +27,23 @@ function getBgmDatabase(dir) {
   return bgmCollection;
 }
 
+function mergeLocaleData(db) {
+  locales.forEach((locale) => {
+    const localeDb = getBgmDatabase(`./locale/${locale}`);
+    localeDb.forEach((song) => {
+      const { filename } = song;
+      const entryInDb = db.find((dbSong) => dbSong.filename === filename);
+      if (entryInDb) {
+        entryInDb.locale = entryInDb.locale || {};
+        entryInDb.locale[locale] = song;
+      }
+    });
+  });
+}
+
 function mergeBgmDatabase(dir) {
   const db = getBgmDatabase(dir);
+  mergeLocaleData(db);
   fs.writeFileSync('bgm.min.json', JSON.stringify(db));
 }
 
